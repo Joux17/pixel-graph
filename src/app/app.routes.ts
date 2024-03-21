a5ac4f66dfbef60ac58b8ec5c979d1c017df434a
Thomas Fajoux <thomas.fajoux@mymoneybank.com>
2024-03-21 14:14:45 +0100

    creation d'une page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { DialogButtonComponent } from './components/dialog-button/dialog-button.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: 'dialog',
     component: DialogButtonComponent
-  }
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  },
+
 ];
